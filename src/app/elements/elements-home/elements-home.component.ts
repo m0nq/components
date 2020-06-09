f96efd8792ed14cda20e87127c9d67b55e60393f
m0nq <forgetmenaught@gmail.com>
2020-06-09 13:02:43 -0700

    Generated basic feature modules with routing

---
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cq-elements-home',
+  templateUrl: './elements-home.component.html',
+  styleUrls: ['./elements-home.component.css']
+})
+export class ElementsHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

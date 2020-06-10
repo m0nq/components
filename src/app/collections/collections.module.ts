afd2932020674a873ee7b6248321315b050568e1
m0nq <forgetmenaught@gmail.com>
2020-06-09 22:34:29 -0700

    Connect together routing modules with defualt routes

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -9,9 +9,6 @@ import { CollectionsHomeComponent } from './collections-home/collections-home.co
     imports: [
         CommonModule,
         CollectionsRoutingModule
-    ],
-    exports: [
-        CollectionsHomeComponent
     ]
 })
 export class CollectionsModule {}

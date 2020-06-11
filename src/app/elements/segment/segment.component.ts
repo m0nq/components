cffc9d8c57a9b0d971a82c49f1ae784fe67ca212
m0nq <forgetmenaught@gmail.com>
2020-06-11 00:01:39 -0700

    Lazy load elements and collections components, add divider widget

---
+++ src/app/elements/segment/segment.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'cq-segment',
+    templateUrl: './segment.component.html',
+    styleUrls: ['./segment.component.css']
+})
+export class SegmentComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
---
+++ src/app/shared/divider/divider.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'cq-divider',
+    templateUrl: './divider.component.html',
+    styleUrls: ['./divider.component.css']
+})
+export class DividerComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}

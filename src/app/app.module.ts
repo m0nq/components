505fbfd806ce5dd6446a9ed0cd8cba913d6029a8
m0nq <forgetmenaught@gmail.com>
2020-06-15 13:12:40 -0700

    Set up router links wihtin collections component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { BiographyComponent } from './collecions/biography/biography.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        BiographyComponent,
         HomeComponent,
         NotFoundComponent
     ],
